27601dfb5006df35af2cbfdd1f8be82e2b0c10e4
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-12-17 21:07:10 +0530

    got some themes and dummy data

---
+++ app/Constants/index.js
@@ -0,0 +1,150 @@
+export const categories = [
+    {
+        id: 1,
+        name: 'Pizza',
+        image: "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg",
+    },
+    {
+        id: 2,
+        name: 'Burger',
+        image: "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg",
+    },
+    {
+        id: 3,
+        name: 'Italian',
+        image: "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg",
+    },
+    {
+        id: 4,
+        name: 'Chinese',
+        image: "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg",
+    },
+    {
+        id: 5,
+        name: 'Noodles',
+        image: "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg",
+    },
+    {
+        id: 6,
+        name: 'Sweets',
+        image: "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg",
+    },
+
+]
+
+
+export const featured = {
+    id: 1,
+    title: 'Hot and Spicy',
+    description: 'soft and tender fried chicken',
+    restaurants: [
+        {
+            id: 1,
+            name: 'Papa Johns',
+            image: "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg",
+            description: 'Hot and spicy pizzas',
+            lng: -85.5324269,
+            lat: 38.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews: '4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                   id: 1,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg"
+                },
+                {
+                   id: 2,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg"
+                },
+                {
+                   id: 3,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg"
+                },
+            ]
+    
+        },
+        {
+            id: 2,
+            name: 'Papa Johns',
+            image: "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg",
+            description: 'Hot and spicy pizzas',
+            lng: -85.5324269,
+            lat: 38.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews: '4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                   id: 1,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg"
+                },
+                {
+                   id: 2,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg"
+                },
+                {
+                   id: 3,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg"
+                },
+            ]
+    
+        },
+        {
+            id: 3,
+            name: 'Papa Johns',
+            image: "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg",
+            description: 'Hot and spicy pizzas',
+            lng: -85.5324269,
+            lat: 38.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews: '4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                   id: 1,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg"
+                },
+                {
+                   id: 2,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg"
+                },
+                {
+                   id: 3,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  "https://res.cloudinary.com/dzgaxsvy2/image/upload/v1699280159/xgjfexiqpj3lzr4b8ju9.jpg"
+                },
+            ]
+    
+        }
+    ]
+}
